a23bcbcbe1719fc8f61691b386155251e4d86b13
Károly Török <torok.karoly.krisztian@gmail.com>
2021-06-14 18:56:37 +0200

    dBank withdraw function

--- src/contracts/dBank.sol
+++ src/contracts/dBank.sol
@@ -12,6 +12,7 @@ contract dBank {
   mapping(address => bool) public isDeposited;
 
   event Deposit(address indexed user, uint etherAmount, uint timeStart);
+  event Withdraw(address indexed user, uint etherAmount, uint timeStart, uint interest);
 
   //pass as constructor argument deployed Token contract
   constructor(Token _token) public {
@@ -31,19 +32,29 @@ contract dBank {
 
   function withdraw() public {
     //check if msg.sender deposit status is true
+    require(isDeposited[msg.sender] == true, 'Deposit not found');
     //assign msg.sender ether deposit balance to variable for event
+    uint userBalance = etherBalanceOf[msg.sender];
 
     //check user's hodl time
+    uint depositTime = block.timestamp - depositStart[msg.sender];
 
     //calc interest per second
+    uint interestPerSecond = 31668017 * (etherBalanceOf[msg.sender] / 1e16);
     //calc accrued interest
+    uint interest = interestPerSecond * depositTime;
 
     //send eth to user
+    msg.sender.transfer(userBalance);
     //send interest in tokens to user
+    token.mint(msg.sender, interest);
 
     //reset depositer data
-
+    etherBalanceOf[msg.sender] = 0;
+    depositStart[msg.sender] = 0;
+    isDeposited[msg.sender] = false;
     //emit event
+    emit Withdraw(msg.sender, userBalance, depositTime, interest);
   }
 
   function borrow() payable public {
